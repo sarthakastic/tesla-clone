37bac19568b4d1a1b75eef8053944c0fdc983f30
sarthak2605 <iamsg26@gmail.com>
2022-07-17 19:19:57 +0530

    Updates

---
+++ precache-manifest.95c3f131d4ce3828ba0acaf21272da77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa8bc85411d8b5f1d6e7",
+    "url": "/static/css/main.e2710c07.chunk.css"
+  },
+  {
+    "revision": "aa8bc85411d8b5f1d6e7",
+    "url": "/static/js/main.aa8bc854.chunk.js"
+  },
+  {
+    "revision": "3da4e6a8535dd6d8027b",
+    "url": "/static/js/1.3da4e6a8.chunk.js"
+  },
+  {
+    "revision": "256946258566b21e1eae",
+    "url": "/static/js/2.25694625.chunk.js"
+  },
+  {
+    "revision": "6cef7b3bb7788b74d6ee",
+    "url": "/static/js/runtime~main.6cef7b3b.js"
+  },
+  {
+    "revision": "be6228998e31904c0d42d148597680fa",
+    "url": "/static/media/logo.be622899.svg"
+  },
+  {
+    "revision": "d234c75740702f9374df3c6f22cf506d",
+    "url": "/index.html"
+  }
+];
